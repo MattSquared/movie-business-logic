6f7df06edec6df3fe95dbf414c529bd0a35cf8c8
carpediem <mat.carpella@gmail.com>
2019-02-06 12:30:03 +0100

    changed REST calls name

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.listen(PORT, () => {
   console.log('Server running on port ' + PORT)
 })
 
-app.get('/getMovieInfo', (req, res, next) => {
+app.get('/movie', (req, res, next) => {
   calls.getMovieInfo(req.query.id, res)
 })
 
-app.post('/sendShowTimes', (req, res, next) => {
+app.post('/mailShowTimes', (req, res, next) => {
   calls.sendMail(req.body.user, req.body.mail, req.body.body, SHOW_TIMES, res)
 })
 
-app.post('/sendShowsTimes', (req, res, next) => {
+app.post('/mailShowsTimes', (req, res, next) => {
   calls.sendMail(req.body.user, req.body.mail, req.body.body, SHOWS_TIMES, res)
 })
